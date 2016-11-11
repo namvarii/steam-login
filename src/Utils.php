cf3d7038de0f779b667db8951fca2c05faa19fc7
Tobias Kronthaler <kronthto@gmail.com>
2016-11-11 18:15:09 +0100

    Removed copyright blocks at beginning of src files

--- src/Utils.php
+++ src/Utils.php
@@ -1,17 +1,5 @@
 <?php
 
-/*
- * This file is part of drdelay/steam-login.
- *
- * (c) DrDelay <info@vi0lation.de>
- *
- * This source file is subject to the MIT license that is bundled with this source code in the file LICENSE.
- */
-
-/**
- * @author DrDelay <info@vi0lation.de>
- */
-
 namespace DrDelay\SteamLogin;
 
 use GuzzleHttp\Client;
